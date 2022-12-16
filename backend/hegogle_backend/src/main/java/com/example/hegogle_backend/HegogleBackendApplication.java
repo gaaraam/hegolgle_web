266283f757a5c0bfe96c9b671c26fef3e9565523
leegaram <leegaaraam@icloud.com>
2022-12-16 17:42:09 +0900

    feat: backend 추가

---
+++ backend/hegogle_backend/src/main/java/com/example/hegogle_backend/HegogleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hegogle_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HegogleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HegogleBackendApplication.class, args);
+	}
+
+}
